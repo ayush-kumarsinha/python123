e4c8b940724785d5f364c39222d2eb41717b9f8d
ayush-kumarsinha <victusayush@gmail.com>
2024-12-30 23:47:55 +0530

    this is my first program

---
+++ distAndSets/chapter/first.py
@@ -0,0 +1,31 @@
+# wriet a pythone program to input 2 number and print their sum.
+"""
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+sum = a + b 
+print("the sum is : " , sum)
+"""
+
+# WAP to input side of a square and pringt its area.
+"""
+side = int(input("sie of square : "))
+area = side * side
+print("side of sqare is : " , area)
+
+"""
+# WAP to input 2 floating number and print their average
+"""
+a = float(input("Enter the first number : "))
+b = float(input("Enter the second number : "))
+average = (a + b)/2
+print("The average value is : " , average)
+"""
+
+# WAP to input 2 int numbers, a and b. print True if a is greater then or equal to b. if not print False.
+
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+if(a >= b):
+    print(True)
+else:
+    print(False)
