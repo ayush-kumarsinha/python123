3a8b014d7415ea1caf0fa7c0adf66fdd15fc6f0f
ayush-kumarsinha <victusayush@gmail.com>
2025-01-02 22:04:18 +0530

    while loop

---
+++ while Loop/02_problem.py
@@ -0,0 +1,7 @@
+# print number from 100 to 1
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+print("Loop ended")
---
+++ while Loop/basic.py
@@ -0,0 +1,10 @@
+# print infinite time for hello
+"""
+while True:
+    print("hello") """
+    
+count = 1
+while count <= 5:
+    print("hello")
+    count += 1
+print(count)    
