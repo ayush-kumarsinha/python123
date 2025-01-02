226417975735e4d92d13087c1d622c5c17e6061e
ayush-kumarsinha <victusayush@gmail.com>
2025-01-02 12:15:42 +0530

    constractor

---
+++ constractor/constractor.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        
+    def welcome(self):
+        print("Welcome student", self.name)
+        
+    def get_marks(self):
+        return self.marks
+
+s1 = Student("ayush", 97)
+print(s1.name, s1.marks)
+
+s2 = Student("Arjun", 88)
+print(s2.get_marks())
